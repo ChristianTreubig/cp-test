a53e0c111723abeca613ee6e5acdde34ddd76bbc
Christian Treubig <ctreubig@eab.com>
2022-01-26 20:02:41 -0500

    cleanup

--- cp_project/insta_clone/models.py
+++ cp_project/insta_clone/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# user0 = User.objects.all()[0]
-# user1 = User.objects.all()[1]
-# Follow.objects.create(from_user=user1, to_user=user0)
-# user0.followers.all()[0].from_user
-# user1.following.all()[0].to_user
 
 class Follow(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
