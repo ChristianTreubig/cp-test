90d0602e7e093e0e2fed98a7bd1ebf8f0738c96c
Christian Treubig <ctreubig@eab.com>
2022-01-27 11:07:18 -0500

    kill imports

--- cp_project/insta_clone/views.py
+++ cp_project/insta_clone/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
